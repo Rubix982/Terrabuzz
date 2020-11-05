a50e1e664f63032ce8147a39d0039483f38418ca
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-11-05 23:29:04 +0500

    post.js updated with components after merge, navbar not added due App.js logic, profile picture needs changes due to public/ folder update hence not rendering

--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import Container from "../components/FullViewContainer.js";
+import MainContent from "../components/Post/MainContent.js";
+import NavBar from "../components/Navbar.js";
 
 const post = ({ children, style }) => {
-    return (
-        <div>
-            POST HERE
-        </div>
-    );
+  return (
+    <Container
+      style={{
+        backgroundColor: "#18191a",
+        display: "grid",
+        gridTemplateRows: "10% 90%",
+      }}
+    >
+      <div></div>
+      <MainContent />
+    </Container>
+  );
 };
 
 export default post;
