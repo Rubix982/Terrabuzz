4d8be43e77e62219bb7d37270acf1b41f355804c
Saif Ul Islam <saifulislam84210@gmail.com>
2020-11-06 16:55:43 +0500

    COMMIT # 3, add navbar to publish page

--- client/src/pages/publish.js
+++ client/src/pages/publish.js
@@ -3,10 +3,12 @@ import React from 'react';
 // Component imports
 import MainContent from './../components/Publish/MainContent';
 import PublishHeader from './../components/Publish/PublishHeader';
+import Navbar from './../components/Navbar';
 
 const publish = ({ children, style }) => {
     return (
         <div>
+            <Navbar />
             <MainContent />
             <PublishHeader />
         </div>
