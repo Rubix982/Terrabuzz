b090358686cf1bd33db77bdd6c3d150231229377
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-11-07 14:31:25 +0500

    - template.js added in Login-Regsiter-FP/ as template page for Login, Register and Forget Password - MainContent.js added containing form logic for Login - global css added for index.js - pages/Login refactored for reusable component - css added for components in styles

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,52 +1,13 @@
-import React from 'react'
-import '../style/Login.css'
-import Logo from '../assets/img/icon/Logo.svg'
+import React from "react";
+import Template from "../components/Login-Register-FP/template.js";
+import MainContent from "../components/Login/MainContent.js";
+
 const Login = (props) => {
+  return (
+    <Template>
+      <MainContent></MainContent>
+    </Template>
+  );
+};
 
-return (
-<div className="page-grid">
-
-    <div className="form-container inner-half-grid-1">
-
-        <div>
-            <img className="logo" src={Logo} alt="" />
-        </div>
-
-        <div>
-            <h1 className="heading-1">
-                We are Terrabuzz
-            </h1>
-
-            <h2 className="heading-2">
-                Please Login To Continue.
-            </h2>
-
-            <form className="form">
-                <input type="text" name="email" placeholder="Email Address"></input> 
-                <input type="password" name="password" placeholder="Password"></input>
-                <input type="checkbox" id="remember-me" name="remember-me" value="remember-me"/>
-                <label className="remember-me"> Remember Me </label>
-                <a href="/forget"  className="forget"> Forgot Password?</a>
-                <br/>
-                <input className="login" type="button" value="Login" />
-                <input className="register" type="button" value="Register" />
-            </form>
-
-        </div>
-
-    </div>
-
-    <div className="content-container">
-        <div className="inner-half-grid-2">
-            <div>
-                <a href="/Contact Us"  className="information-list">  Contact </a>
-                <a href="/About"  className="information-list"> About Us </a>
-            </div>
-        </div>
-    </div>
-
-</div>
-)
-}
-
-export default Login
+export default Login;
