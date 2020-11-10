cb5142c22a56a77fcebb75c09f9759592288ed64
Tashik Moin <tashik.moin@studentpartner.com>
2020-11-10 11:20:47 +0500

    Added Settings Page

--- client/src/pages/settings.js
+++ client/src/pages/settings.js
@@ -3,11 +3,109 @@ import React from 'react';
 import Navbar from '../components/Navbar'
 
 const Settings = ({ children, style }) => {
+
     return (
-        <div>
-          <Navbar first_name="Tashik" title={"Terrabuzz"} />
-            SETTINGS HERE
+
+    <div>
+        <Navbar first_name="Tashik" title={"Terrabuzz"} />
+        <div className="settings-page-grid">
+
+            <div className="settings-sidebar">
+                <input type="button" className="previous-button" value="Previous" /> 
+                <div className="settings-sidebar-controls-grid" >
+                    <div>
+                        <input type="button" className="current options-button account-preferences" 
+                        value="Account Preferences" />
+                    </div>
+
+                    <div>
+                        <input type="button" className="options-button security" value="Security" />
+                    </div>                
+
+                    <div>
+                        <input type="button" className="options-button visibility" value="Visibility" />
+                    </div>            
+
+                    <div>
+                        <input type="button" className="options-button communication" value="Communication" />
+                    </div>             
+
+                    <div>
+                        <input type="button" className="options-button data-privacy" value="Data Privacy" />
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="settings-fields">
+
+                <div className="settings-fields-input" >
+
+                    <div className="input-fields"> 
+
+                        <div className="align-input-field-1">
+                            <h3> Email Address </h3>
+                        </div>
+
+                        <div className="align-input-field-2">
+                            <input className="search-box" type="text"  />
+                        </div>
+                        
+                    </div>
+
+                    <div className="input-fields"> 
+
+                        <div className="align-input-field-1">
+                            <h3> Username </h3>
+                        </div>
+
+                        <div className="align-input-field-2">
+                            <input className="search-box" type="text"  />
+                        </div>
+
+                    </div>
+
+                    <div className="input-fields"> 
+                        <div className="align-input-field-1">
+                            <h3> User handler </h3>
+                        </div>
+
+                        <div className="align-input-field-2">
+                            <input className="search-box" type="text"  />
+                        </div>
+                    </div>
+
+                    <div className="input-fields"> 
+                        <div className="align-input-field-1">
+                            <h3> Password </h3>
+                        </div>
+
+                        <div className="align-input-field-2">
+                            <input className="search-box" type="text"  />
+                        </div>
+                    </div>
+
+                    <div className="input-fields"> 
+                        <div className="align-input-field-1">
+                            <h3> Confirm Password </h3>
+                        </div>
+
+                        <div className="align-input-field-2">
+                            <input className="search-box" type="text"  />
+                        </div>
+                    </div>
+
+                    <div className="input-fields">
+                        <input className="update-button" type="button" value="Save Changes" />
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
+
+    </div>
     )
 };
 
