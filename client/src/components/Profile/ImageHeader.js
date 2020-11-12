8e6f3de4fc2ff00ce508b1a7757650f9201b2c88
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-11-12 20:36:53 +0500

    profile picture fix

--- client/src/components/Profile/ImageHeader.js
+++ client/src/components/Profile/ImageHeader.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import ImageHeaderStyling from '../../style/Profile/ImageHeader.module.css'
-import PlusSymbol from '../../assets/profile/plus.svg'
-import MainProfilePicture from '../../assets/profile/Profile_Picture.jpg';
+import ImageHeaderStyling from "../../style/Profile/ImageHeader.module.css";
+import PlusSymbol from "../../assets/profile/plus.svg";
+import MainProfilePicture from "../../assets/profile/Profile_Picture.jpg";
 
 const ImageHeader = ({ children, style }) => {
-    return (
-        <div className={ImageHeaderStyling.cover}>
-            <div className={ImageHeaderStyling.profilePicture}>
-                <img className={ImageHeaderStyling.picture} src={MainProfilePicture} alt="" name='profile-picture' />
-            </div>
-            <div className={ImageHeaderStyling.plusSymbolPicture}>
-                <img className={ImageHeaderStyling.plusSymbol} src={PlusSymbol} alt="" name='plus-symbol' />
-            </div>
-        </div>
-    );
+  return (
+    <div className={ImageHeaderStyling.cover}>
+      <div className={ImageHeaderStyling.profilePicture}>
+        <img src={MainProfilePicture} alt="" name="profile-picture" />
+      </div>
+      <div className={ImageHeaderStyling.plusSymbolPicture}>
+        <img
+          className={ImageHeaderStyling.plusSymbol}
+          src={PlusSymbol}
+          alt=""
+          name="plus-symbol"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ImageHeader;
