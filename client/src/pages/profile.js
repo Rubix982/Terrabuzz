708f96a0d4f151abe5ae8afd7698a129c5570e9c
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-11-12 17:42:06 +0500

    Refactored and fixed view of Profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-// import Navbar from '../components/Navbar'
-import MainContent from '../components/Profile/MainContent';
+import Container from "../components/FullViewContainer.js";
+import Navbar from "../components/Navbar.js";
+import ImageHeader from "../components/Profile/ImageHeader.js";
+import MainContent from "../components/Profile/MainContent";
 
 const Profile = ({ children, style }) => {
-    return (
-        <MainContent />
-    );
+  return (
+    <Container style={{ display: "grid", gridTemplateRows: "10% 30% 60%" }}>
+      <Navbar />
+      <ImageHeader />
+      <MainContent />
+    </Container>
+  );
 };
 
 export default Profile;
