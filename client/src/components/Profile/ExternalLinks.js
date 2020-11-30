f51a87f2841582e2efedde4292b7352fa1233fdf
Saif Ul Islam <saifulislam84210@gmail.com>
2020-11-30 17:33:42 +0500

    option added to change the links - modal remain

--- client/src/components/Profile/ExternalLinks.js
+++ client/src/components/Profile/ExternalLinks.js
@@ -8,34 +8,27 @@ import LinkedInLogo from "../../assets/profile/linkedin.svg"
 import WebsiteLogo from "../../assets/profile/web.svg"
 import YoutubeLogo from "../../assets/profile/youtube.svg"
 import TwitterLogo from "../../assets/profile/twitter.svg"
+import EditLogo from '../../assets/profile/edit.svg';
 
 // React Component
-import Link from './Link'
+import SocialMediaFeedLink from './SocialMediaFeedLink'
 
 const ExternalLinks = ({ children, style }) => {
 
-    const InstragramLink = "instagram.com/tashikmoinshaikh/",
-        FacebookLink = "facebook.com/TashikMoinSheikh",
-        LinkedInLink = "linkedin.com/in/tashik-moin-sheikh-08872116b/",
-        WebsiteLink = "dev-hearted.software",
-        YoutubeLink = "youtube.com/channel/UC_bxQjn16KukkyWqTY_MZvg",
-        TwitterLink = "twitter.com/TashikMSheikh";
-
-    const Instagram = { Logo: InstagramLogo, Link: InstragramLink };
-    const Facebook = { Logo: FacebookLogo, Link: FacebookLink };
-    const Website = { Logo: WebsiteLogo, Link: WebsiteLink };
-    const Youtube = { Logo: YoutubeLogo, Link: YoutubeLink };
-    const Twitter = { Logo: TwitterLogo, Link: TwitterLink };
-    const LinkedIn = { Logo: LinkedInLogo, Link: LinkedInLink };
-
-    return (    
-        <div className={ExternalLinksStyling.externalLinkLayout}>
-            <Link props={Youtube} />
-            <Link props={Twitter} />
-            <Link props={LinkedIn} />
-            <Link props={Instagram} />
-            <Link props={Facebook} />
-            <Link props={Website} />
+    return (
+        <div className={ExternalLinksStyling.GridSystem}>
+            <div className={ExternalLinksStyling.externalLinkLayout}>
+                <SocialMediaFeedLink logo={InstagramLogo} link="instagram.com/tashikmoinshaikh/" />
+                <SocialMediaFeedLink logo={LinkedInLogo} link="linkedin.com/in/tashik-moin-sheikh-08872116b/" />
+                <SocialMediaFeedLink logo={FacebookLogo} link="facebook.com/TashikMoinSheikh" />
+                <SocialMediaFeedLink logo={WebsiteLogo} link="dev-hearted.software" />
+                <SocialMediaFeedLink logo={YoutubeLogo} link="youtube.com/channel/UC_bxQjn16KukkyWqTY_MZvg" />
+                <SocialMediaFeedLink logo={TwitterLogo} link="twitter.com/TashikMSheikh" />
+            </div>
+            <div className={ExternalLinksStyling.EditOption}>
+                <img src={EditLogo} alt="" />
+                <p>Edit?</p>
+            </div>
         </div>
     );
 };
---
+++ client/src/components/Profile/SocialMediaFeedLink.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import LinkStyling from '../../style/Profile/SocialMediaFeedLink.module.css'
+
+const SocialMediaFeedLink = ({ link, logo }) => {
+    
+    const hrefLink = "https://" + link;
+    
+    return (
+        <div className={LinkStyling.Layout}>
+            <img src={logo} alt='' />
+            <a href={hrefLink}>{link}</a>
+        </div>
+    );
+};
+
+export default SocialMediaFeedLink;
