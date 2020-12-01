f57d163766c1c1add7a50f15ed7207e69f544017
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-01 16:15:41 +0500

    updated Search/ components based on lint errors

--- client/src/components/Search/InterestContent.js
+++ client/src/components/Search/InterestContent.js
@@ -1,60 +1,59 @@
-import React from "react";
-import ComponentStyling from "../../style/Search/Content.module.css";
-import Post from "./Post.js";
-import Profile from "./InterestProfile.js";
-import { useContext } from "react";
-import { searchTypeContext } from "./SearchTypeContext.js";
+import React, { useContext } from 'react';
+import ComponentStyling from '../../style/Search/Content.module.css';
+import Post from './Post';
+import Profile from './InterestProfile';
+
+import { searchTypeContext } from './SearchTypeContext';
 
 const InterestContent = () => {
   const [buttonActive] = useContext(searchTypeContext);
 
-  if (buttonActive === "Posted on") {
+  if (buttonActive === 'Posted on') {
     return (
       <div className={ComponentStyling.content}>
         <div className={ComponentStyling.postRow}>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </div>
         <div className={ComponentStyling.postRow}>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </div>
         <div className={ComponentStyling.postRow}>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-          <Post></Post>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className={ComponentStyling.content}>
-        <div className={ComponentStyling.postRow}>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
-        </div>
-        <div className={ComponentStyling.postRow}>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
-        </div>
-        <div className={ComponentStyling.postRow}>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
-          <Profile></Profile>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </div>
       </div>
     );
   }
+  return (
+    <div className={ComponentStyling.content}>
+      <div className={ComponentStyling.postRow}>
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+      <div className={ComponentStyling.postRow}>
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+      <div className={ComponentStyling.postRow}>
+        <Profile />
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
+    </div>
+  );
 };
 
 export default InterestContent;
--- client/src/components/Search/SearchTypeContext.js
+++ client/src/components/Search/SearchTypeContext.js
@@ -1,9 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState } from 'react';
 
 export const searchTypeContext = createContext();
 
 const SearchTypeProvider = ({ children }) => {
-  const [buttonActive, setButtonActive] = useState("Interested in");
+  const [buttonActive, setButtonActive] = useState('Interested in');
 
   return (
     <searchTypeContext.Provider value={[buttonActive, setButtonActive]}>
