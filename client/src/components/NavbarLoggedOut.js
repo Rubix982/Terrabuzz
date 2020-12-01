4dd99bf34c4a34830847f2b4a1452378fff62c32
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-01 16:16:15 +0500

    updated other components based on lint errors

--- client/src/components/NavbarLoggedOut.js
+++ client/src/components/NavbarLoggedOut.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React from 'react';
 
-import Logo from "../assets/img/icon/Logo.svg";
-import ComponentStyling from "../style/NavbarLoggedOut.module.css";
-import { CenterAlign, RightAlign } from "./FlexAlignment.js";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/img/icon/Logo.svg';
+import ComponentStyling from '../style/NavbarLoggedOut.module.css';
+import { CenterAlign, RightAlign } from './FlexAlignment';
 
-const NavbarLoggedOut = (props) => {
-  return (
-    <div className={ComponentStyling.navbar}>
-      <div className={ComponentStyling.leftContent}>
-        <RightAlign>
-          <div className={ComponentStyling.logo}>
-            <img className="logo" src={Logo} alt="" />
-          </div>
-        </RightAlign>
-        <div className={ComponentStyling.title}>
-          <Link to="/">
-            <h1>Terrabuzz</h1>
-          </Link>
+const NavbarLoggedOut = () => (
+  <div className={ComponentStyling.navbar}>
+    <div className={ComponentStyling.leftContent}>
+      <RightAlign>
+        <div className={ComponentStyling.logo}>
+          <img className="logo" src={Logo} alt="" />
         </div>
-      </div>
-      <div className={ComponentStyling.rightContent}>
-        <CenterAlign style={{ alignItems: "flex-end" }}>
-          <div className={ComponentStyling.loginButton}>
-            <Link to="/login">Login</Link>
-          </div>
-        </CenterAlign>
-        <CenterAlign style={{ alignItems: "flex-end" }}>
-          <div className={ComponentStyling.registerButton}>
-            <Link to="/register">Register</Link>
-          </div>
-        </CenterAlign>
+      </RightAlign>
+      <div className={ComponentStyling.title}>
+        <Link to="/">
+          <h1>Terrabuzz</h1>
+        </Link>
       </div>
     </div>
-  );
-};
+    <div className={ComponentStyling.rightContent}>
+      <CenterAlign style={{ alignItems: 'flex-end' }}>
+        <div className={ComponentStyling.loginButton}>
+          <Link to="/login">Login</Link>
+        </div>
+      </CenterAlign>
+      <CenterAlign style={{ alignItems: 'flex-end' }}>
+        <div className={ComponentStyling.registerButton}>
+          <Link to="/register">Register</Link>
+        </div>
+      </CenterAlign>
+    </div>
+  </div>
+);
 export default NavbarLoggedOut;
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,26 +1,30 @@
-import React from 'react'
-const Posts = ( {user_number: { user_name, avatar_url, url } } ) =>  {
-        return (
-            <div className="container card text-center">
-                <img className="round-img" src={avatar_url} alt="" style={{ width: '60px'}} />
+import React from 'react';
 
-                <h3> {user_name} </h3>
+const Posts = ({ user_number: { userName, avatarUrl, url } }) => (
+  <div className="container card text-center">
+    <img className="round-img" src={avatarUrl} alt="" style={{ width: '60px' }} />
 
-                <div> 
-                    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                        when an unknown printer took a galley of type and scrambled it to make a type 
-                        specimen book.
-                    </p>
-                </div>
+    <h3>
+      {' '}
+      {userName}
+      {' '}
+    </h3>
 
-                <div>
-                <a className="btn btn-dark btn-sm my-1" href={url} >
-                    View Complete Post
-                </a>
-                </div>
-            </div>
-        )
-}
-export default Posts
+    <div>
+      <p>
+        {' '}
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+        specimen book.
+      </p>
+    </div>
 
+    <div>
+      <a className="btn btn-dark btn-sm my-1" href={url}>
+        View Complete Post
+      </a>
+    </div>
+  </div>
+);
+export default Posts;
