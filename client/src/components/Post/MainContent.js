fcf9abb9892b84a0893351a4e4004fd477f3835b
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-01 16:13:46 +0500

    updated Post/ components based on lint errors

--- client/src/components/Post/MainContent.js
+++ client/src/components/Post/MainContent.js
@@ -1,18 +1,16 @@
-import React from "react";
-import { CenterAlign } from "../FlexAlignment.js";
-import ComponentStyling from "../../style/Post/MainContent.module.css";
-import PostContent from "./PostContent.js";
-import CommentContent from "./CommentContent.js";
+import React from 'react';
+import { CenterAlign } from '../FlexAlignment';
+import ComponentStyling from '../../style/Post/MainContent.module.css';
+import PostContent from './PostContent';
+import CommentContent from './CommentContent';
 
-const MainContent = () => {
-  return (
-    <CenterAlign>
-      <main className={ComponentStyling.main}>
-        <PostContent />
-        <CommentContent />
-      </main>
-    </CenterAlign>
-  );
-};
+const MainContent = () => (
+  <CenterAlign>
+    <main className={ComponentStyling.main}>
+      <PostContent />
+      <CommentContent />
+    </main>
+  </CenterAlign>
+);
 
 export default MainContent;
