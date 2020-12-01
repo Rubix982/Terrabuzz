79394b230f7d2341c86ffc30666f03d1ad95703a
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-01 16:16:40 +0500

    updated pages based on lint errors

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,8 +1,6 @@
-import React from "react";
-import MainContent from "../components/AboutUs/MainContent";
+import React from 'react';
+import MainContent from '../components/AboutUs/MainContent';
 
-const AboutUs = ({ childen, style }) => {
-  return <MainContent />;
-};
+const AboutUs = () => <MainContent />;
 
 export default AboutUs;
