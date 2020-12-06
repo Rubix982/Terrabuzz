a0627a43571452dee0ab142c4b142b7084e3c2cd
tashikmoin23 <tashikmoinsheikh@gmail.com>
2020-12-06 14:23:28 -0800

    Added insertion js file

--- server/db/mysql/insert-sql.js
+++ server/db/mysql/insert-sql.js
@@ -2,16 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const MYSQL_CONNECTOR = (require('./connection.js'));
 
-const create = () => {
-  const script = fs.readFileSync(path.join(__dirname, './ddl.sql')).toString();
+const insert = () => {
+  const script = fs.readFileSync(path.join(__dirname, './insertSql.sql')).toString();
   MYSQL_CONNECTOR.connection.query(script, (err, result) => {
     if (err) {
       console.log(`Query Not Executed Successfully Because --> ${err}`);
     } else {
-      console.log('Database Created Successfully');
+      console.log('Insertions Done Successfully');
       console.log(result);
     }
   });
 };
 
-module.exports.create = create;
+module.exports.insert = insert;
