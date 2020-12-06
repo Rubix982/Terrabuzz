db0779308779ea99dcd677f0725a01a8d3dc94fe
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-06 06:42:07 +0500

    API endpoints added with dummy contollers

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,17 @@ const Router = express.Router();
 const indexContoller = require('../controllers/index.js');
 
 Router.get('/', indexContoller.getHomePage);
+Router.get('/feed', indexContoller.getUserFeed);
+Router.get('/profile/:id', indexContoller.getUserProfile);
+Router.post('publish', indexContoller.publishPost);
+Router.get('/post/:id', indexContoller.getPost);
+Router.get('/search-post', indexContoller.searchPost);
+Router.get('/search-user', indexContoller.searchUser);
+Router.get('/settings', indexContoller.getSettings);
+Router.put('/settings', indexContoller.updateSettings);
+Router.post('/login', indexContoller.loginUser);
+Router.post('/register', indexContoller.registerUser);
+Router.post('/new-password', indexContoller.newPassword);
+Router.post('/reset-password', indexContoller.resetPassword);
 
 module.exports = Router;
