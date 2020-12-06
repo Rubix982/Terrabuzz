86cfa323d942e6258b4731d29414fc8bfb479bae
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-07 02:40:27 +0500

    added required global middlewares

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const indexRouter = require('./routes/index.js');
 const middleware = require('./middleware/index.js');
 const MYSQL_CONNECTOR = (require('./db/mysql/connection.js'));
@@ -13,6 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use(cookieParser());
+app.use(cors({ credentials: true }));
 app.use('/', indexRouter);
 app.use(middleware.notFound);
 app.use(middleware.onError);
