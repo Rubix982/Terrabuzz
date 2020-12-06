3a035a1185eb1558f878c2c0b560d322a2dd7726
tashikmoin23 <tashikmoinsheikh@gmail.com>
2020-12-06 09:46:48 -0800

    Refactored

--- server/app.js
+++ server/app.js
@@ -2,28 +2,15 @@ const express = require('express');
 const indexRouter = require('./routes/index.js');
 const middleware = require('./middleware/index.js');
 const MYSQL_CONNECTOR = (require('./db/mysql/connection.js'));
+const CreateSQL = (require('./db/mysql/create-sql.js'));
 const MONGOOSE_CONNECTOR = require('./db/mongo/connection.js');
-const path = require('path');
-const fs = require('fs');
 const { Console } = require('console');
 require('dotenv').config();
 
-const Declare_Schema = () => {
-    {
-        const queries = fs.readFileSync(path.join(__dirname, './db/mysql/ddl.sql')).toString();
-        const query = MYSQL_CONNECTOR.connection.query(queries,  (err, result) => {
-        if(err){
-            console.log(`Query Not Executed Successfully Because --> ${err}`);
-        }
-        else{
-            console.log(`Database Created Successfully`);
-        }
-        });
-    }
-}
+
 
 MYSQL_CONNECTOR.connect();
-Declare_Schema();
+CreateSQL.create();
 MONGOOSE_CONNECTOR.connect();
 
 const app = express();
