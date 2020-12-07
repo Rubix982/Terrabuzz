de9bd0dcbe3ee949ae91a1655c2dd81ac73fc8ed
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-07 19:38:31 +0500

    mongoose model refactoring

--- server/models/post.js
+++ server/models/post.js
@@ -4,37 +4,44 @@ const { Schema } = mongoose;
 
 const Post = new Schema({
   /**
-     * @property handler: From the 'User' table.
      * This is for the User Handler of the person that creates the post
      * @property title: Meant to be the title for the post
-     * @property _id: For the Post ID. This should be auto incremented
      * @property: datePublished: The date that the post was published on
-     * @property: contentRefId: Contains the hash for the compressed content
+     * @property: content: Contains the hash for the compressed content
      */
-  handler: {
-    type: String,
-    max: 40,
-    required: true,
-  },
   title: {
     type: String,
     max: 40,
     required: true,
   },
-  _id: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
   dataPublished: {
     type: Date,
     required: true,
   },
-  contentRefId: {
+  content: {
     type: String,
     max: 100,
     required: true,
   },
+  like: {
+    type: [Schema.ObjectID],
+  },
+  comment: {
+    type: [Schema.ObjectID],
+  },
+});
+
+const PostList = new Schema({
+  _id: {
+    type: String,
+    max: 40,
+    required: true,
+  },
+  payload: {
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  },
 });
 
 module.exports = mongoose.model('Post', Post);
+module.exports = mongoose.model('PostList', PostList);
