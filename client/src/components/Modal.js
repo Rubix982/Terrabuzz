5833dfc36195e088c3f2fa4c5c8e820c2aaa8e5c
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-10 10:38:46 +0500

    resolving rebase

---
+++ client/src/components/Modal.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+import ModalStyling from '../style/Modal.module.css';
+
+const Modal = () => {
+
+};
+
+export default Modal;
--- client/src/components/Profile/ProfileSection.js
+++ client/src/components/Profile/ProfileSection.js
@@ -1,4 +1,9 @@
 import React from 'react';
+
+// React components
+import EditInfoComponent from './EditInfoComponent';
+
+// Styling
 import ProfileSectionStyling from '../../style/Profile/ProfileSection.module.css';
 
 const ProfileSection = ({ ProfileSectionInformation }) => (
@@ -40,44 +45,8 @@ const ProfileSection = ({ ProfileSectionInformation }) => (
         <p>{ProfileSectionInformation.Interest}</p>
       </div>
     </div>
+    <EditInfoComponent />
   </div>
-
-  // <div className={ProfileSectionStyling.flexLayout}>
-  //   <div className={ProfileSectionStyling.layoutClass}>
-  //     <p className={ProfileSectionStyling.UserName}>{children.UserName}</p>
-  //     <p className={ProfileSectionStyling.UserHandle}>
-  //       {children.UserHandle}
-  //     </p>
-  //     <p className={ProfileSectionStyling.Location}>{children.Location}</p>
-  //     <div className={ProfileSectionStyling.borderRadius}>
-  //       <div className={ProfileSectionStyling.ConnectionPostBreaker}>
-  //         <p
-  //           className={ProfileSectionStyling.Connections}
-  //           style={{ display: "inline-block" }}
-  //         >
-  //           {children.Connections} Connections
-  //         </p>
-  //         <p
-  //           className={ProfileSectionStyling.Posts}
-  //           style={{ display: "inline-block" }}
-  //         >
-  //           {children.Posts} Posts
-  //         </p>
-  //       </div>
-  //       <p className={ProfileSectionStyling.Biography}>
-  //         {children.Biography}
-  //       </p>
-  //     </div>
-  //     <div className={ProfileSectionStyling.FixSpacingIssue}>
-  //       <p className={ProfileSectionStyling.ActivityHeader}>Activity</p>
-  //       <p className={ProfileSectionStyling.Activities}>
-  //         {children.Activities}
-  //       </p>
-  //       <p className={ProfileSectionStyling.InterestHeader}>Interest</p>
-  //       <p className={ProfileSectionStyling.Interest}>{children.Interest}</p>
-  //     </div>
-  //   </div>
-  // </div>
 );
 
 export default ProfileSection;
