b9ad0db5df26ba0b873829edccddeebf597dbd39
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-10 05:32:33 +0500

    updated MainContent.js to action that calls API and logs in user based on email and password also redirects to /feed

--- client/src/components/Login/MainContent.js
+++ client/src/components/Login/MainContent.js
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { loginUser } from '../../services/login'
 import ComponentStyling from '../../style/Login/MainContent.module.css';
 
-const MainContent = () => (
-  <div className={ComponentStyling.content}>
-    <h1>We are Terrabuzz</h1>
+const MainContent = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
 
-    <h2>Please login to continue</h2>
+  return (
+    <div className={ComponentStyling.content}>
+      <h1>We are Terrabuzz</h1>
 
-    <form className={ComponentStyling.formlogin}>
-      <input type="text" name="email" placeholder="Email Address" />
-      <input type="password" name="password" placeholder="Password" />
-      <input
-        type="checkbox"
-        id="remember-me"
-        name="remember-me"
-        value="remember-me"
-      />
-      <label className={ComponentStyling.rememberMe}> Remember Me </label>
-      <a href="/forget" className={ComponentStyling.forget}>
-        Forgot Password?
-      </a>
-      <br />
-      <input className={ComponentStyling.login} type="button" value="Login" />
-      <input
-        className={ComponentStyling.register}
-        type="button"
-        value="Register"
-      />
-    </form>
-  </div>
-);
+      <h2>Please login to continue</h2>
+
+      <form className={ComponentStyling.formlogin}>
+        <input onChange={(event)=>{
+          setEmail(event.target.value);
+        }} value={email} type="text" name="email" placeholder="Email Address" />
+        <input onChange={(event)=>{
+          setPassword(event.target.value);
+        }} value={password} type="password" name="password" placeholder="Password" />
+        <input
+          type="checkbox"
+          id="remember-me"
+          name="remember-me"
+          value="remember-me"
+        />
+        <label className={ComponentStyling.rememberMe}> Remember Me </label>
+        <a href="/forget" className={ComponentStyling.forget}>
+          Forgot Password?
+        </a>
+        <br />
+        <input onClick={ async()=>{
+          try {
+            await loginUser(email,password);
+            localStorage.setItem('loggedIn', true);
+            setTimeout(()=>{
+              history.push('/feed');
+            },3000);
+          } catch (error) {
+            alert(error.message);
+          }
+        }} className={ComponentStyling.login} type="button" value="Login" />
+        <Link to='/register'>
+         <input
+          className={ComponentStyling.register}
+          type="button"
+          value="Register"
+         />
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default MainContent;
