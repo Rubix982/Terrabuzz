50d7da729886236d6ab9ad334b01a662e886680d
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-11 18:41:56 +0500

    added hamburger dropdown

--- client/src/components/Profile/ImageHeader.js
+++ client/src/components/Profile/ImageHeader.js
@@ -3,9 +3,18 @@ import React from 'react';
 import ImageHeaderStyling from '../../style/Profile/ImageHeader.module.css';
 import PlusSymbol from '../../assets/profile/plus.svg';
 import MainProfilePicture from '../../assets/profile/Profile_Picture.jpg';
+import CameraPicture from '../../assets/profile/camera.svg';
 
 const ImageHeader = () => (
   <div className={ImageHeaderStyling.cover}>
+    {/* <div className={ImageHeaderStyling.cameraSymbolPicture}>
+      <img
+        className={ImageHeaderStyling.cameraSymbol}
+        src={CameraPicture}
+        alt=''
+        name='camera-symbol'
+      />
+    </div> */}
     <div className={ImageHeaderStyling.profilePicture}>
       <img src={MainProfilePicture} alt="" name="profile-picture" />
     </div>
