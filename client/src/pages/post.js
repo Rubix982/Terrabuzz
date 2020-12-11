8a21579d70e7ff7d0877f0d33c8562d187c7b0df
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-12 04:28:39 +0500

    conditionally rendering navbar on basis of login status of user

--- client/src/pages/post.js
+++ client/src/pages/post.js
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from '../components/FullViewContainer';
 import MainContent from '../components/Post/MainContent';
 import PostProvider from '../components/Post/PostContext';
+import { loginUserContext } from '../components/LoginUserContext';
 
 
 import Navbar from '../components/Navbar';
+import NavBarLoggedOut from '../components/NavbarLoggedOut';
 
-const Post = () => (
-  <Container
-    style={{
-      backgroundColor: '#18191a',
-      display: 'grid',
-      gridTemplateRows: '10% 90%',
-    }}
-  >
-    <Navbar first_name="Tashik" title="Terrabuzz" />
-    <PostProvider>
-      <MainContent />    
-    </PostProvider>
-  </Container>
-);
-
+const Post = () => {
+  const [ loggedIn ] = useContext(loginUserContext);
+  return (
+    <Container
+      style={{
+        backgroundColor: '#18191a',
+        display: 'grid',
+        gridTemplateRows: '10% 90%',
+      }}
+    >
+      { loggedIn ? <Navbar first_name="Tashik" title="Terrabuzz" /> : <NavBarLoggedOut/> }
+      <PostProvider>
+        <MainContent />    
+      </PostProvider>
+    </Container>
+  );
+};
 export default Post;
