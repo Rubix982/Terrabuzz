188f0fed65cd898dba7dc2cc7545b095ab03a650
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-11 05:51:26 +0500

    populated homepage components with data coming from API

--- client/src/components/Home/Content.js
+++ client/src/components/Home/Content.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import API from '../../API/API';
+import { Link } from 'react-router-dom';
 import ComponentStyling from '../../style/Home/Content.module.css';
 import Post from './Post';
+require('dotenv').config();
 
-const Content = () => (
-  <div className={ComponentStyling.content}>
-    <Post />
-    <Post />
-    <Post />
-  </div>
-);
+const Content = () => {
+  const [postData, setPostData] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect( async ()=> {
+    const data = await API.getRequest(`${process.env.REACT_APP_API_URL}/`);
+    setPostData(data);
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+      </>
+    );
+  } else {
+    return (
+      <div className={ComponentStyling.content}>
+        {postData.map((element) => {
+          return (
+            <Link key={element._id} to={'/post/' + element._id}>
+            <Post key={element._id} handle={element.handle} date={element.datePublished} content={element.content} likeCount={element.likes.length} commentCount={element.comments.length} />
+            </Link>
+           );
+        })}
+      </div>
+    );
+  }
+};
 
 export default Content;
