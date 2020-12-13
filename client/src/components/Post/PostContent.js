d79b0095b58ead1ce30914ada2d5858c71178fa3
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-13 23:09:34 +0500

    added conditional rendering on like toggle, and used valid like counts service for total likes

--- client/src/components/Post/PostContent.js
+++ client/src/components/Post/PostContent.js
@@ -2,9 +2,13 @@ import React, { useContext } from 'react';
 import { postContext } from './PostContext';
 import ComponentStyling from '../../style/Post/PostContent.module.css';
 import { CenterAlign } from '../FlexAlignment';
+import LikeToggle from './LikeToggle';
+import { countTotalLikes } from '../../services/post';
+import { loginUserContext } from '../LoginUserContext';
 
 const PostContent = () => {
-  const [[ postData , setPostData ],,] = useContext(postContext);
+  const [ loggedIn ] = useContext(loginUserContext);
+  const { post } = useContext(postContext);
 
   return (
     <div className={ComponentStyling.main}>
@@ -19,29 +23,34 @@ const PostContent = () => {
             </div>
           </CenterAlign>
           <div className={ComponentStyling.postTitle}>
-            <h1>Richard Miles | @{postData.handle}</h1>
-            <h1>{postData.datePublished}</h1>
+            <h1>Richard Miles | @{post.state.handle}</h1>
+            <h1>{post.state.datePublished}</h1>
           </div>
         </div>
         <div className={ComponentStyling.postText}>
           <div>
             <h1>
-              {postData.title}
+              {post.state.title}
             </h1>
           </div>
           <div>
             <p>
-              {postData.content}
+              {post.state.content}
             </p>
           </div>
         </div>
       </div>
       <div className={ComponentStyling.stats}>
+        { loggedIn ? (
+          <LikeToggle/>
+        ) : (
         <CenterAlign>
-          <h1>{postData.likes.length} Likes</h1>
+          <h1>{countTotalLikes(post.state.likes)} Likes</h1>
         </CenterAlign>
+        )}
+
         <CenterAlign>
-          <h1>{postData.comments.length} Comments</h1>
+          <h1>{post.state.comments.length} Comments</h1>
         </CenterAlign>
       </div>
     </div>
