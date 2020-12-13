5dc85334553f59d486851b14fe3dcaf651c88cd7
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-14 04:03:19 +0500

    added reducePostContent service for Post

--- client/src/services/post.js
+++ client/src/services/post.js
@@ -1,3 +1,10 @@
 export const countTotalLikes = (__likes) => {
   return (__likes.filter((element) => (element.isLiked === true))).length;
 };
+
+export const reducePostContent = (__content) => {
+  if (__content.length > 200) {
+    return `${__content.substr(0,200)} .....`;
+  }
+  return __content;
+};
