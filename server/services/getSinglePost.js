a06dc9f40056766b3b23e58ae1d043c18bf7fb4d
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-13 23:16:04 +0500

    updated model fetch in both services to .populate()

--- server/services/getSinglePost.js
+++ server/services/getSinglePost.js
@@ -1,7 +1,7 @@
 const { Post } = require('../models/post.js');
 
 const getSinglePost = async (__postID) => {
-  const post = await Post.findById(__postID).populate('comments');
+  const post = await Post.findById(__postID).populate('likes');
   return (post);
 };
 
--- server/services/homePage.js
+++ server/services/homePage.js
@@ -2,7 +2,7 @@ const { Post } = require('../models/post.js');
 
 const getHomePagePosts = async () => {
   try {
-    const fetchedPosts = await Post.find({}).sort({ _id: 1 }).limit(10);
+    const fetchedPosts = await Post.find({}).populate('likes').sort({ _id: 1 }).limit(10);
     return fetchedPosts;
   } catch (error) {
     throw new Error(error.message);
