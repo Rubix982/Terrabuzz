80eae5d1982f911b5b8312b2f6915a482fbe33ff
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-16 13:39:25 +0500

    refactored register component

--- client/src/components/Register/MainContent.js
+++ client/src/components/Register/MainContent.js
@@ -1,7 +1,7 @@
-import React , {useRef} from 'react';
+import React, { useRef } from 'react';
 import ComponentStyling from '../../style/Register/register.module.css';
-
-
+import { registerUserDetails } from '../../services/register';
+require('dotenv').config();
 
 const MainContent = () => {
 
@@ -11,45 +11,21 @@ const MainContent = () => {
   const _username = useRef('');
   const _userhandler = useRef('');
 
-  const insertUser = (Options) => 
-  {
-    fetch('http://localhost:8080/api/register', Options)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error)) ; 
-  }
-    
-    
-  const Register = (event) =>
-  {
+  const registerUser = async (event) => {
     event.preventDefault();
-    	
-    let formData = 
-    {
-      email: _email.current.value,
-      password: _password.current.value,
-      cpassword: _cpassword.current.value,
-      username: _username.current.value,
-      userhandler: _userhandler.current.value
-    } ;
-        
-    const Options = 
-    {
-      method: 'POST',
-      headers: 
-      { 
-        'Accept': 'application/json', 
-        'Content-Type': 'application/json', 
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
-      },
-      body: JSON.stringify(formData)
-    };
 
-    insertUser(Options) ;
+    try {
+      await registerUserDetails(
+        _email.current.value,
+        _password.current.value,
+        _cpassword.current.value,
+        _username.current.value,
+        _userhandler.current.value
+      );
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
-
-
 
   return (
     <div className={ComponentStyling.content}>
@@ -59,7 +35,7 @@ const MainContent = () => {
       </div>
       <div>
         <form className={ComponentStyling.registerForm}>
-          <input type="text" ref={_email} placeholder="Email Address"  />
+          <input type="text" ref={_email} placeholder="Email Address" />
           <input type="password" ref={_password} placeholder="Password" />
           <input
             type="password"
@@ -76,7 +52,7 @@ const MainContent = () => {
             ref={_userhandler}
             placeholder="Userhandle e.g @johndoe"
           />
-          <a href="/forget-password"> Already have an account?</a>
+          <a href="/login"> Already have an account?</a>
           <br />
           <input type="checkbox" name="remember-me" value="remember-me" />
           <label htmlFor="remember-me" style={{ fontSize: '16px' }}>
@@ -90,7 +66,7 @@ const MainContent = () => {
             className={ComponentStyling.registerButton}
             type="Submit"
             value="Register"
-            onClick={Register}
+            onClick={registerUser}
           />
         </form>
       </div>
