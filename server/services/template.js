a4ace77df677783be6494cdaeb3e602ced4a0d40
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-17 15:44:53 +0500

    added better ui template for reset password

--- server/services/template.js
+++ server/services/template.js
@@ -1,4 +1,4 @@
-const HTMLResetPasswordForm = `
+const HTMLResetPasswordForm = (__hashedLink) => (`
 <head>
     <title>Rating Reminder</title>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
@@ -42,17 +42,28 @@ const HTMLResetPasswordForm = `
 
 <body style="background-color: #f4f4f5;">
     <table cellpadding="0" cellspacing="0"
-        style="width: 100%; height: 100%; background-color: #f4f4f5; text-align: center;">
+        style="width: 100%; 
+        height: 100%; 
+        background-color: #f4f4f5; 
+        text-align: center;">
         <tbody>
             <tr>
                 <td style="text-align: center;">
                     <table align="center" cellpadding="0" cellspacing="0" id="body"
-                        style="background-color: #fff; width: 100%; max-width: 680px; height: 100%;">
+                        style="background-color: #fff; 
+                        width: 100%; 
+                        max-width: 680px; 
+                        height: 100%;">
                         <tbody>
                             <tr>
                                 <td>
                                     <table align="center" cellpadding="0" cellspacing="0" class="page-center"
-                                        style="text-align: left; padding-bottom: 88px; width: 100%; padding-left: 120px; padding-right: 120px;">
+                                        style="text-align: left;
+                                        padding-bottom: 88px;
+                                        width: 100%;
+                                        padding-left: 120px;
+                                        padding-right: 120px;
+                                        ">
                                         <tbody>
                                             <tr>
                                                 <td style="padding-top: 24px;">
@@ -62,7 +73,21 @@ const HTMLResetPasswordForm = `
                                             </tr>
                                             <tr>
                                                 <td colspan="2"
-                                                    style="padding-top: 72px; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #000000; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 48px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: -2.6px; line-height: 52px; mso-line-height-rule: exactly; text-decoration: none;">
+                                                    style="padding-top: 72px; 
+                                                    -ms-text-size-adjust: 100%; 
+                                                    -webkit-font-smoothing: 
+                                                    antialiased; 
+                                                    -webkit-text-size-adjust: 100%; 
+                                                    color: #000000; 
+                                                    font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; 
+                                                    font-size: 48px; 
+                                                    font-smoothing: always; 
+                                                    font-style: normal; 
+                                                    font-weight: 600; 
+                                                    letter-spacing: -2.6px; 
+                                                    line-height: 52px;
+                                                    mso-line-height-rule: exactly; 
+                                                    text-decoration: none;">
                                                     Reset your password</td>
                                             </tr>
                                             <tr>
@@ -71,7 +96,11 @@ const HTMLResetPasswordForm = `
                                                         <tbody>
                                                             <tr>
                                                                 <td
-                                                                    style="width: 100%; height: 1px; max-height: 1px; background-color: #d9dbe0; opacity: 0.81">
+                                                                    style="width: 100%;
+                                                                    height: 1px;
+                                                                    max-height: 1px;
+                                                                    background-color: #d9dbe0;
+                                                                    opacity: 0.81">
                                                                 </td>
                                                             </tr>
                                                         </tbody>
@@ -80,21 +109,75 @@ const HTMLResetPasswordForm = `
                                             </tr>
                                             <tr>
                                                 <td
-                                                    style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                                    style="-ms-text-size-adjust: 100%;
+                                                    -ms-text-size-adjust: 100%;
+                                                    -webkit-font-smoothing: antialiased;
+                                                    -webkit-text-size-adjust: 100%;
+                                                    color: #9095a2;
+                                                    font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+                                                    font-size: 16px;
+                                                    font-smoothing: always;
+                                                    font-style: normal;
+                                                    font-weight: 400;
+                                                    letter-spacing: -0.18px;
+                                                    line-height: 24px;
+                                                    mso-line-height-rule: exactly;
+                                                    text-decoration: none;
+                                                    vertical-align: top;
+                                                    width: 100%;
+                                                    ">
                                                     You're receiving this e-mail because you requested a password reset
                                                     for your Terrabuzz account.
                                                 </td>
                                             </tr>
                                             <tr>
                                                 <td
-                                                    style="padding-top: 24px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095a2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 16px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: -0.18px; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                                    style="padding-top: 24px;
+                                                    -ms-text-size-adjust: 100%;
+                                                    -ms-text-size-adjust: 100%;
+                                                    -webkit-font-smoothing: antialiased;
+                                                    -webkit-text-size-adjust: 100%;
+                                                    color: #9095a2;
+                                                    font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+                                                    font-size: 16px;
+                                                    font-smoothing: always;
+                                                    font-style: normal;
+                                                    font-weight: 400;
+                                                    letter-spacing: -0.18px;
+                                                    line-height: 24px;
+                                                    mso-line-height-rule: exactly;
+                                                    text-decoration: none;
+                                                    vertical-align: top;
+                                                    width: 100%;
+                                                    ">
                                                     Please tap the button below to choose a new password.
                                                 </td>
                                             </tr>
                                             <tr>
                                                 <td>
-                                                    <a data-click-track-id="37" href="#"
-                                                        style="margin-top: 36px; -ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #ffffff; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 12px; font-smoothing: always; font-style: normal; font-weight: 600; letter-spacing: 0.7px; line-height: 48px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 220px; background-color: #00cc99; border-radius: 28px; display: block; text-align: center; text-transform: uppercase"
+                                                    <a data-click-track-id="37" href="${process.env.CLIENT_URL}/newPassword/${__hashedLink}"
+                                                        style="margin-top: 36px; 
+                                                        -ms-text-size-adjust: 100%; 
+                                                        -ms-text-size-adjust: 100%; 
+                                                        -webkit-font-smoothing: antialiased; 
+                                                        -webkit-text-size-adjust: 100%; 
+                                                        color: #ffffff; 
+                                                        font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; 
+                                                        font-size: 12px; 
+                                                        font-smoothing: always; 
+                                                        font-style: normal; 
+                                                        font-weight: 600; 
+                                                        letter-spacing: 0.7px; 
+                                                        line-height: 48px; 
+                                                        mso-line-height-rule: exactly; 
+                                                        text-decoration: none; 
+                                                        vertical-align: top; 
+                                                        width: 220px; 
+                                                        background-color: #00cc99; 
+                                                        border-radius: 28px; 
+                                                        display: block; 
+                                                        text-align: center; 
+                                                        text-transform: uppercase"
                                                         target="_blank">
                                                         Reset Password
                                                     </a>
@@ -112,7 +195,11 @@ const HTMLResetPasswordForm = `
                             <tr>
                                 <td>
                                     <table align="center" cellpadding="0" cellspacing="0" class="footer-center"
-                                        style="text-align: left; width: 100%; padding-left: 120px; padding-right: 120px;">
+                                        style="text-align: left;
+                                        width: 100%;
+                                        padding-left: 120px;
+                                        padding-right: 120px;
+                                        ">
                                         <tbody>
                                             <tr>
                                                 <td colspan="2"
@@ -121,12 +208,19 @@ const HTMLResetPasswordForm = `
                                                 </td>
                                             </tr>
                                             <tr>
-                                                <td colspan="2" style="padding-top: 24px; padding-bottom: 48px;">
+                                                <td colspan="2" 
+                                                style="
+                                                padding-top: 24px; 
+                                                padding-bottom: 48px;">
                                                     <table cellpadding="0" cellspacing="0" style="width: 100%">
                                                         <tbody>
                                                             <tr>
                                                                 <td
-                                                                    style="width: 100%; height: 1px; max-height: 1px; background-color: #EAECF2; opacity: 0.19">
+                                                                    style="width: 100%; 
+                                                                    height: 1px; 
+                                                                    max-height: 1px; 
+                                                                    background-color: #EAECF2; 
+                                                                    opacity: 0.19">
                                                                 </td>
                                                             </tr>
                                                         </tbody>
@@ -135,7 +229,22 @@ const HTMLResetPasswordForm = `
                                             </tr>
                                             <tr>
                                                 <td
-                                                    style="-ms-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: 100%; color: #9095A2; font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; font-size: 15px; font-smoothing: always; font-style: normal; font-weight: 400; letter-spacing: 0; line-height: 24px; mso-line-height-rule: exactly; text-decoration: none; vertical-align: top; width: 100%;">
+                                                    style="-ms-text-size-adjust: 100%; 
+                                                    -ms-text-size-adjust: 100%; 
+                                                    -webkit-font-smoothing: antialiased; 
+                                                    -webkit-text-size-adjust: 100%; 
+                                                    color: #9095A2; 
+                                                    font-family: 'Postmates Std', 'Helvetica', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif; 
+                                                    font-size: 15px; 
+                                                    font-smoothing: always; 
+                                                    font-style: normal; 
+                                                    font-weight: 400; 
+                                                    letter-spacing: 0; 
+                                                    line-height: 24px; 
+                                                    mso-line-height-rule: exactly; 
+                                                    text-decoration: none; 
+                                                    vertical-align: top; 
+                                                    width: 100%;">
                                                     If you have any questions or concerns, we're here to help. Contact
                                                     us via our <a data-click-track-id="1053" href="#"
                                                         style="font-weight: 500; color: #ffffff" target="_blank">Help
@@ -156,6 +265,6 @@ const HTMLResetPasswordForm = `
         </tbody>
     </table>
 </body>
-`;
+`);
 
 module.exports.HTMLResetPasswordForm = HTMLResetPasswordForm;
