b2cb3549d6a7264612d4b3e16b03892dad1876bb
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-18 01:32:57 +0500

    add related services for new and reset password

--- client/src/services/newPassword.js
+++ client/src/services/newPassword.js
@@ -1,19 +1,31 @@
 import API from '../API/API';
 require('dotenv').config();
 
-export const newPasswordGenerator = async (__email) => {
-    if (!__email || !__password) {
-        throw new Error('Email fields cannot be empty');
+export const newPasswordGenerator = async (__hashed) => {
+    if (!__hashed) {
+        throw new Error('Hash field cannot be empty');
     }
 
-
-
     try {
-        const data = await
-            API.postRequest(`${process.env.REACT_APP_API_URL}/login`,
-                { email: __email, password: __password });
-        console.log(data.msg);
+        const data = await API.getRequest(`${process.env.REACT_APP_API_URL}/newPassword/${__hashed}`)
+        return data;
     } catch (error) {
         throw new Error(error.message);
     }
 };
+
+export const changePasswordWithHandle = async (__hashed, __handle, __toChangePasswordWith) => {
+    if (!__hashed || !__handle || !__toChangePasswordWith) {
+        throw new Error('Required fields cannot be empty!');
+    }
+
+    try {
+        return await API.postRequest(`${process.env.REACT_APP_API_URL}/newPassword/${__hashed}`, {
+            handle: __handle,
+            password: __toChangePasswordWith
+        });
+    } catch (error) {
+        console.log(error.message)
+        throw new Error(error.message);
+    }
+}
