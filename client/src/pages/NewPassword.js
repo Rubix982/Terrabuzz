e19b4c4d217b417ce7bdc23ea9d1c480d823addc
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-18 01:32:25 +0500

    remove new password funtionality from the page component of new password

--- client/src/pages/NewPassword.js
+++ client/src/pages/NewPassword.js
@@ -1,19 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import API from '../API/API';
+import React from 'react';
 import MainContent from '../components/NewPassword/MainContent';
 
 const NewPassword = () => {
-
-  const { hashed } = useParams();
-
-  useEffect(async () => {
-    try {
-      const { status } = await API.getRequest(`${process.env.REACT_APP_API_URL}/newPassword/${hashed}`);
-    } catch (error) {
-      alert(error.message);
-    }
-  }, []);
 
   return (
     <MainContent />
