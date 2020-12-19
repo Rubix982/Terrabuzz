58e7e398fa13c6ae1563b4a04499418055ea4ee3
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-19 05:02:23 +0500

    updated static InterestContent with PostedOnEntries which renders entries in rows

--- client/src/components/Search/InterestContent.js
+++ client/src/components/Search/InterestContent.js
@@ -1,57 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import ComponentStyling from '../../style/Search/Content.module.css';
-import Post from './Post';
-import Profile from './InterestProfile';
-
-import { searchTypeContext } from './SearchTypeContext';
+import InterestedInEntries from './InterestedInEntries';
+import PostedOnEntries from './PostedOnEntries';
+import { searchContext } from './SearchTypeContext';
 
 const InterestContent = () => {
-  const [buttonActive] = useContext(searchTypeContext);
-
-  if (buttonActive === 'Posted on') {
-    return (
-      <div className={ComponentStyling.content}>
-        <div className={ComponentStyling.postRow}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+  const { buttonActive } = useContext(searchContext);
+  
+  if (buttonActive.state === 'Posted on') {
+      return (
+        <div className={ComponentStyling.content}>
+          <PostedOnEntries />
         </div>
-        <div className={ComponentStyling.postRow}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-        <div className={ComponentStyling.postRow}>
-          <Post />
-          <Post />
-          <Post />
-          <Post />
-        </div>
-      </div>
-    );
+      );
   }
   return (
     <div className={ComponentStyling.content}>
-      <div className={ComponentStyling.postRow}>
-        <Profile />
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
-      <div className={ComponentStyling.postRow}>
-        <Profile />
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
-      <div className={ComponentStyling.postRow}>
-        <Profile />
-        <Profile />
-        <Profile />
-        <Profile />
-      </div>
+      <InterestedInEntries />
     </div>
   );
 };
