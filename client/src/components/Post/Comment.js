5d7fef7a21ea4f226e31168ca675791207c589e3
Muhammad Hassan Zahid <hassanzhd2000@gmail.com>
2020-12-21 22:50:17 +0500

    updated ui side of comment rendering on individual posts

--- client/src/components/Post/Comment.js
+++ client/src/components/Post/Comment.js
@@ -2,26 +2,29 @@ import React from 'react';
 import ComponentStyling from '../../style/Post/Comment.module.css';
 import { CenterAlign } from '../FlexAlignment';
 
-const Comment = () => (
-  <div className={ComponentStyling.entry}>
-    <CenterAlign>
-      <div className={ComponentStyling.profilePicture}>
-        <img src="/assets/img/profile_pictures/boy(2).svg" alt="user profile" />
-      </div>
-    </CenterAlign>
-    <div className={ComponentStyling.commentBody}>
-      <div className={ComponentStyling.commentTitle}>
-        <h1>Richard Miles</h1>
-        <h2>1st January, 2020</h2>
-      </div>
-      <div className={ComponentStyling.commentText}>
-        <p>
-          Yesterday is history, and tomorrow is a mystery, but today, is a
-          gift, that is why we call the present.
-        </p>
+const Comment = ({ commentData }) => {
+  const { handle, ProfilePicture, Username, content, datePosted } = commentData; 
+
+  return (
+    <div className={ComponentStyling.entry}>
+      <CenterAlign>
+        <div className={ComponentStyling.profilePicture}>
+          <img src={`/assets/img/profile_pictures/${ProfilePicture}`} alt="user profile" />
+        </div>
+      </CenterAlign>
+      <div className={ComponentStyling.commentBody}>
+        <div className={ComponentStyling.commentTitle}>
+          <h1>{Username} | @{handle}</h1>
+          <h2>{datePosted}</h2>
+        </div>
+        <div className={ComponentStyling.commentText}>
+          <p>
+            {content}
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Comment;
