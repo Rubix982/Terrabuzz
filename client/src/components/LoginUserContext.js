91925e63ca7315d155761a5e220fb527fb91d302
Tashik <k180142@nu.edu.pk>
2020-12-26 00:48:35 +0500

    restored hassan's changes

--- client/src/components/LoginUserContext.js
+++ client/src/components/LoginUserContext.js
@@ -1,26 +1,12 @@
 import React, { createContext, useState, useEffect } from 'react';
-import API from '../API/API';
 
 export const loginUserContext = createContext();
 
 const LoginUserProvider = ({ children }) => {
   const loginState = useState(false);
-  // const userName = useState('');
-  // const profilePicturePath = useState('');
-
 
   useEffect(() => {
     (loginState[1])((localStorage.getItem('loggedIn') === 'true') ? true : false);
-    // if(loginState[0] === true) 
-    // {
-    //   try {
-    //     const response = await API.getRequest(`${process.env.REACT_APP_API_URL}/feed`);
-    //     const [data] = response[0];
-    //     return data;
-    //   } catch (error) {
-    //       throw new Error(error.message);
-    //   }
-    // }
   }, [loginState[0]])
 
   return (
