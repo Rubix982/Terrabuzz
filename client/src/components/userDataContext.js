6730a95d91648c0e5c9ec22dbb938f74223b2506
Tashik <k180142@nu.edu.pk>
2020-12-27 02:52:47 +0500

    fixed navbar styling

--- client/src/components/userDataContext.js
+++ client/src/components/userDataContext.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 const UserContext = React.createContext();
 
 export const UserProvider = (props) => {
 
     const [userName , setUserName ] = useState('Tashik Moin');
     const [imageSource , setImageSource ] = useState('/assets/img/profile_pictures/boy(3).svg');
+
+    useEffect( () => {} , [] )
 
     return(
         <UserContext.Provider value={{userName, imageSource}}>
