9e02c6a1c3b45161b67ec33740bbd80b81b2e55b
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-27 00:10:28 +0500

    add import of postContext for rebasing stage

--- client/src/components/Post/NewComment.js
+++ client/src/components/Post/NewComment.js
@@ -1,25 +1,26 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import ComponentStyling from '../../style/Post/NewComment.module.css';
 import { useParams } from 'react-router-dom'
 import { CenterAlign } from '../FlexAlignment';
 import API from '../../API/API.js';
+import { postContext } from './PostContext';
 
-
-const NewComment = () => 
-{
+const NewComment = () => {
   const [_Comment, setComment] = useState('');
   const { id } = useParams();
+  const { post } = useContext(postContext);
 
   const newComment = async event => {
 
     event.preventDefault();
-    let Data = { Comment : _Comment } ;
+    let Data = { Comment: _Comment };
 
     if (!_Comment) {
       alert('Field cannot be empty!')
     } else {
       try {
-        await API.postRequest(`${process.env.REACT_APP_API_URL}/post/${id}`, Data );
+        await API.postRequest(`${process.env.REACT_APP_API_URL}/post/${id}`, Data);
+        alert('Comment successfully made!')
       } catch (error) {
         alert(`Unable to comment, sorry! Error is ${error.message}`)
       }
@@ -27,28 +28,28 @@ const NewComment = () =>
   }
 
   return (
-  <div className={ComponentStyling.entry}>
-    <CenterAlign>
-      <div className={ComponentStyling.profilePicture}>
-        <img src="/assets/img/profile_pictures/boy(2).svg" alt="user profile" />
-      </div>
-    </CenterAlign>
-    <div className={ComponentStyling.commentBody}>
-      <input 
-      type="text" 
-      name="_Comment" 
-      placeholder="Add a comment ..."
-      value={_Comment}
-      onChange={event => setComment(event.target.value)}/>
+    <div className={ComponentStyling.entry}>
+      <CenterAlign>
+        <div className={ComponentStyling.profilePicture}>
+          <img src="/assets/img/profile_pictures/boy(2).svg" alt="user profile" />
+        </div>
+      </CenterAlign>
+      <div className={ComponentStyling.commentBody}>
+        <input
+          type="text"
+          name="_Comment"
+          placeholder="Add a comment ..."
+          value={_Comment}
+          onChange={event => setComment(event.target.value)} />
 
-    </div>
-    <CenterAlign>
-      <div className={ComponentStyling.submitButton} onClick={newComment}>
-        Submit
       </div>
-    </CenterAlign>
-  </div>
-);
+      <CenterAlign>
+        <div className={ComponentStyling.submitButton} onClick={newComment}>
+          Submit
+      </div>
+      </CenterAlign>
+    </div>
+  );
 }
 
 export default NewComment;
