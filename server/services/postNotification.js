137c532da1fe5602f5aee91e5226f8004e7757b2
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-28 00:33:51 +0500

    complete post notification unit

--- server/services/postNotification.js
+++ server/services/postNotification.js
@@ -1,27 +1,27 @@
-const { Notification, NotificationList } = require('../models/notification.js');
+const { CommentNotification, LikeNotification } = require('../models/notification.js');
 const mysql = require('../db/mysql/connection.js');
 
-const postNotification = async (__notificationSchemaForm, __userHandle) => {
+const postNotification = async (__action, __notificationSchemaForm, __userHandle) => {
+  if (__notificationSchemaForm.for === __userHandle) {
+    return;
+  }
+
   try {
     const query = `SELECT ProfilePicture FROM TERRABUZZ.UserInformation WHERE Handle='${__userHandle}'`;
     const result = await mysql.connection.query(query);
     const profilePictureFetch = result[0][0].ProfilePicture;
-
+    console.log(__notificationSchemaForm);
     /* eslint-disable no-param-reassign */
     __notificationSchemaForm.by = __userHandle;
     __notificationSchemaForm.profilePicture = profilePictureFetch;
 
-    console.log(__notificationSchemaForm);
-
-    const notificationListSchemaObject = NotificationList.findById(__notificationSchemaForm.for);
-
-    delete __notificationSchemaForm.for;
-
-    const notificationSchemaObject = new Notification(__notificationSchemaForm);
-
-    notificationListSchemaObject.payload.push(notificationSchemaObject);
-
-    await notificationListSchemaObject.save();
+    if (__action === 'comment') {
+      const commentNotification = new CommentNotification(__notificationSchemaForm);
+      await commentNotification.save();
+    } else if (__action === 'like') {
+      const likeNotification = new LikeNotification(__notificationSchemaForm);
+      await likeNotification.save();
+    }
   } catch (error) {
     throw new Error(`Unable to push notification to database, due to error ${error.message}`);
   }
