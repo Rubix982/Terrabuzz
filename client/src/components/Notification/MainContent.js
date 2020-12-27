a4c44e39f43d7894f9af38298ead77a40e30cad7
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-28 02:14:49 +0500

    front end related changes

--- client/src/components/Notification/MainContent.js
+++ client/src/components/Notification/MainContent.js
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // React componenets
-import {ContentComment, ContentLike} from './Content';
+import { ContentComment, ContentLike } from './Content';
 import ContentHead from './ContentHead';
 import { LeftAlign, RightAlign } from '../FlexAlignment';
+
+// Service
+import { getCommentList, getLikeList } from '../../services/getNotifs.js';
 
 // Styling
 import MainContentStyling from '../../style/Notification/MainContent.module.css';
 
-const MainContent = () => (
-  <div className={MainContentStyling.MainContentContainer}>
-    <div className={MainContent.ColumnContentContainer}>
-      <LeftAlign>
-        <ContentHead content='Notifications' />
-      </LeftAlign>
-      <LeftAlign>
-        <ContentComment content='Richard Miles'/>
-      </LeftAlign>
-    </div>
-    <div className={MainContent.ColumnContentContainer}>
-      <RightAlign>
-        <ContentHead content='Likes' />
-      </RightAlign>
-      <RightAlign>
-        <ContentLike content='Richard Miles'/>
-      </RightAlign>
-    </div>
-  </div>
-);
+const MainContent = () => {
+
+  let [loading, setLoading] = useState(true);
+  let [commentList, setCommentList] = useState({}), [likeList, setLikeList] = useState({});
+
+  useEffect(async () => {
+    try {
+      let data = await getCommentList();
+      setCommentList(JSON.stringify(data));
+      data = await getLikeList();
+      setLikeList(JSON.stringify(data));
+      setLoading(false);
+    } catch (error) {
+      alert(error.message);
+    }
+  })
+
+  if (loading) {
+    return (<> </>)
+  } else {
+    return (
+      <div className={MainContentStyling.MainContentContainer}>
+        <div className={MainContent.ColumnContentContainer}>
+          <LeftAlign>
+            <ContentHead content='Comments' />
+          </LeftAlign>
+          <LeftAlign>
+            <ContentComment content={commentList} />
+          </LeftAlign>
+        </div>
+        <div className={MainContent.ColumnContentContainer}>
+          <RightAlign>
+            <ContentHead content='Likes' />
+          </RightAlign>
+          <RightAlign>
+            <ContentLike content={likeList} />
+          </RightAlign>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default MainContent;
--- client/src/components/Notification/SingleNotif.js
+++ client/src/components/Notification/SingleNotif.js
@@ -2,54 +2,58 @@ import React from 'react';
 import SingleNotifStyling from '../../style/Notification/SingleNotif.module.css';
 import { CenterAlign } from '../FlexAlignment';
 
-const SingleNotifComment = ({ content }) => (
-  <div className={SingleNotifStyling.entry}>
-    <div className={SingleNotifStyling.main}>
-      <CenterAlign>
-        <div className={SingleNotifStyling.profilePicture}>
-          <img alt="user icon" src="/assets/img/profile_pictures/boy(3).svg" />
-        </div>
-      </CenterAlign>
-      <div className={SingleNotifStyling.postBody}>
-        <div className={SingleNotifStyling.postText}>
-          <p>
-            <span className={SingleNotifStyling.UserNameHandle}>{content} </span>
+const SingleNotifComment = ({ profilePicture, userHandle, post }) => (
+  <a href={`/post/${post}`}>
+    <div className={SingleNotifStyling.entry}>
+      <div className={SingleNotifStyling.main}>
+        <CenterAlign>
+          <div className={SingleNotifStyling.profilePicture}>
+            <img alt="user icon" src={`/assets/img/profile_pictures/${profilePicture}`} />
+          </div>
+        </CenterAlign>
+        <div className={SingleNotifStyling.postBody}>
+          <div className={SingleNotifStyling.postText}>
+            <p>
+              <span className={SingleNotifStyling.UserNameHandle}>@{userHandle} </span>
             commented on your post!
           </p>
-        </div>
-        <div className={SingleNotifStyling.timeStampInfo}>
-          <p>
-            30th December, 2020
+          </div>
+          <div className={SingleNotifStyling.timeStampInfo}>
+            <p>
+              30th December, 2020
           </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </a>
 );
 
-const SingleNotifLike = ({ content }) => (
-  <div className={SingleNotifStyling.entry}>
-    <div className={SingleNotifStyling.main}>
-      <CenterAlign>
-        <div className={SingleNotifStyling.profilePicture}>
-          <img alt="user icon" src="/assets/img/profile_pictures/boy(3).svg" />
-        </div>
-      </CenterAlign>
-      <div className={SingleNotifStyling.postBody}>
-        <div className={SingleNotifStyling.postText}>
-          <p>
-            <span className={SingleNotifStyling.UserNameHandle}>{content} </span>
+const SingleNotifLike = ({ profilePicture, userHandle, post }) => (
+  <a href={`/post/${post}`}>
+    <div className={SingleNotifStyling.entry}>
+      <div className={SingleNotifStyling.main}>
+        <CenterAlign>
+          <div className={SingleNotifStyling.profilePicture}>
+            <img alt="user icon" src={`/assets/img/profile_pictures/${profilePicture}`} />
+          </div>
+        </CenterAlign>
+        <div className={SingleNotifStyling.postBody}>
+          <div className={SingleNotifStyling.postText}>
+            <p>
+              <span className={SingleNotifStyling.UserNameHandle}>{userHandle} </span>
             liked your post!
           </p>
-        </div>
-        <div className={SingleNotifStyling.timeStampInfo}>
-          <p>
-            30th December, 2020
+          </div>
+          <div className={SingleNotifStyling.timeStampInfo}>
+            <p>
+              30th December, 2020
            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </a>
 );
 
 export { SingleNotifComment, SingleNotifLike };
