22aff2a2755c753ba330661ab7aeda189ecb1bb0
Saif Ul Islam <saifulislam84210@gmail.com>
2020-12-27 17:11:00 +0500

    add proper image tags to the search results

--- client/src/components/Search/Profile.js
+++ client/src/components/Search/Profile.js
@@ -3,20 +3,22 @@ import { Link } from 'react-router-dom'
 import ComponentStyling from '../../style/Search/Profile.module.css';
 import { CenterAlign } from '../FlexAlignment';
 
-const Profile = ({ username, handle }) => (
-  <Link to={`/profile/${handle}`}>
-    <div className={ComponentStyling.entry}>
-    <CenterAlign>
-      <div className={ComponentStyling.profilePicture}>
-        <img src="/assets/img/profile_pictures/boy(3).svg" alt="profilePic" />
+const Profile = ({ username, handle, profilePicture }) => {
+  return (
+    <Link to={`/profile/${handle}`}>
+      <div className={ComponentStyling.entry}>
+        <CenterAlign>
+          <div className={ComponentStyling.profilePicture}>
+            <img src={`/assets/img/profile_pictures/${profilePicture}`} alt="profilePic" />
+          </div>
+        </CenterAlign>
+        <div className={ComponentStyling.details}>
+          <h1>{username}</h1>
+          <h2>@{handle}</h2>
+        </div>
       </div>
-    </CenterAlign>
-    <div className={ComponentStyling.details}>
-      <h1>{username}</h1>
-      <h2>@{handle}</h2>
-    </div>
-    </div>
-  </Link>
-);
+    </Link>
+  )
+};
 
 export default Profile;
